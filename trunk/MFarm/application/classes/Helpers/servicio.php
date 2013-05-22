bf8f909ac4514f01cc20c5727aeeb798048b0689
rumyhuampas@gmail.com <rumyhuampas@gmail.com@6ddd1903-a9cf-4fa2-abf9-825a260a30a3>
2013-05-22 02:13:06 +0000

    report refactor

--- trunk/MFarm/application/classes/Helpers/servicio.php
+++ trunk/MFarm/application/classes/Helpers/servicio.php
@@ -14,6 +14,18 @@ class Helpers_Servicio {
 		}
 	}
 	
+	public static function getByDates($desde, $hasta, $IdCerda = NULL){
+		$qry = DB::select('*', DB::expr('(SELECT Numero FROM cerdas WHERE cerdas.id=servicios.IdCerda) AS Numero'))
+			->from('servicios')
+			->where(DB::expr('DATE(FechaServicio)'), '>=', DB::expr('DATE("'.$desde.'")'))
+			->and_where(DB::expr('DATE(FechaServicio)'), '<=', DB::expr('DATE("'.$hasta.'")'));
+		if($IdCerda != NULL){
+			$qry->and_where('IdCerda', '=', $IdCerda);
+		}
+		$qry->order_by('FechaServicio', 'ASC');
+		return $qry->execute()->as_array();
+	}
+	
 	public static function getLast($IdCerda, $desde = NULL){
 		return ORM::factory('servicio')
 			->select(array(DB::expr('(SELECT Numero FROM cerdas WHERE cerdas.id=servicio.IdCerda)'), 'Numero'))
